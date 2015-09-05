f18ae3acfe8169b99a3e6317ff1882c4763bc917
Matt Jenkins <matt@majenko.co.uk>
2015-09-05 20:41:57 +0100

    Initial import

---
+++ src/org/uecide/themes/Nocturne.java
@@ -0,0 +1,7 @@
+package org.uecide.themes;
+
+public class Nocturne extends ThemeControl {
+    public static void init() {
+        loadFont("/org/uecide/fonts/nk57-monospace-no-rg.ttf");
+    }
+}
